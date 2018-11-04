ed978a60b04b7779b9a79a90956377a77eabd9a2
wang0937 <991817426@qq.com>
2018-11-04 16:29:02 +0800

    js3

--- js2/js/js3-1.js
+++ js2/js/js3-1.js
@@ -1,44 +1,79 @@
-// 获取发牌时保存的数据
-var data = JSON.parse(localStorage.getItem('key'));
 
-var backhome = document.getElementById("backhome");
+// var backhome = document.getElementById("backhome");
+// // 返回按钮返回到发牌页面
+// backhome.onclick = function () {
+//     location.href = "../html/js2-2.html";
+// }
+
+
+// var off = document.getElementById("off");
+// // 关闭按钮返回到主页面
+// off.onclick = function () {
+//     // 点击关闭弹出提示窗口
+//     if (confirm("是否要退出游戏返回到主页面")) {
+//         location.href = "../html/js2-1.html"; //点击确定返回到主页面
+//     } else {
+//         return false; //点击取消停留在当前页面
+//     }
+// }
+
+
 // 返回按钮返回到发牌页面
-backhome.onclick = function () {
+$('#backhome').click(function () {
     location.href = "../html/js2-2.html";
-}
+})
 
-var off = document.getElementById("off");
 // 关闭按钮返回到主页面
-off.onclick = function () {
-    // 点击关闭弹出提示窗口
+$('#off').click(function () {
     if (confirm("是否要退出游戏返回到主页面")) {
         location.href = "../html/js2-1.html"; //点击确定返回到主页面
     } else {
         return false; //点击取消停留在当前页面
     }
-}
-
-var arr = data; //把获取到本地的数据传到数组里
-console.log(arr)
+})
 
 
-var people = document.getElementsByClassName("people");
-var identity = document.getElementsByClassName("identity");
-var amount = document.getElementsByClassName("amount");
+
+// var arr = data; //把获取到本地的数据传到数组里
+// console.log(arr)
 
 
-var i = 0;
+// var people = document.getElementsByClassName("people");
+// var identity = document.getElementsByClassName("identity");
+// var amount = document.getElementsByClassName("amount");
 
-// while (i < arr.length) {
+
+// var i = 0;
+
+// // while (i < arr.length) {
+// //     people[i].style.display = "block";
+// //     identity[i].innerText = arr[i];
+// //     amount[i].innerText = (i+1 + "号");
+// //     i=i+1;  
+// // } 
+
+// i = arr.length
+// for( i>0; i-- ;){
 //     people[i].style.display = "block";
 //     identity[i].innerText = arr[i];
 //     amount[i].innerText = (i+1 + "号");
-//     i=i+1;  
-// } 
+// }
 
-i = arr.length
-for( i>0; i-- ;){
-    people[i].style.display = "block";
-    identity[i].innerText = arr[i];
-    amount[i].innerText = (i+1 + "号");
+// 获取发牌时保存的数据
+var data = JSON.parse(localStorage.getItem('key'));
+var arr = []; //声明一个数组
+var arr = data; //把获取的数据传递到数组
+console.log(arr);
+
+// 循环出玩家对应的盒子
+for (let i = 0; i < arr.length - 1; i++) {
+    $('main').append($('.people').eq(0).clone(true)) //在main里面循环对应的玩家盒子
+}
+
+var x = 0; //声明一个变量记录玩家序号
+// 循环玩家角色
+for (let i = 0; i <= arr.length - 1; i++) {
+    $('.identity').eq(i).text(arr[i]) //把数组指标对应的玩家对象类型属性传入对应指标的盒子里
+    x = i + 1; //获取数组指标对应的玩家对象的玩家序号
+    $('.amount').eq(i).text(x + "号"); //把玩家人数序号传入对应指标的盒子
 }
